40d3ae6e747db11f9b263e5fb04bae78f239d0dd
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-17 15:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const student = {
+  name: "John"
+};
+
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+
+window.student = student;
+window.getKeys = getKeys;
